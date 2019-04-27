f4fe7ee9c0049920b183cf380dc4b731810fa96a
lxm <lxm@192.168.9.112>
2019-04-27 17:22:18 +0800

    lxm:首页的我要投资

---
+++ Credit_Loan/src/main/resources/static/lxm/js/shouye.js
@@ -0,0 +1,10 @@
+function touzi(){
+	$(".right-box-touzi").css("display","block");
+	$(".right-box-shouye").css("display","none");
+}
+		
+function shouye(){
+	$(".right-box-touzi").css("display","none");
+	$(".right-box-shouye").css("display","block");
+}
+

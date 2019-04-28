203b99fdb0751ae621fee612ec543eb856875c56
Administrator <Administrator@PC-20180703NNCD.mshome.net>
2019-04-28 17:00:01 +0800

    第一次

--- Credit_Loan/src/main/java/com/p2p/qiyun/CreditLoanApplication.java
+++ Credit_Loan/src/main/java/com/p2p/qiyun/CreditLoanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreditLoanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CreditLoanApplication.class, args);
+		SpringApplication.run(CreditLoanApplication.class, args); 
 	}
 
 }

9a21a1f1d89790ad12b22097aeb202efa02d3db6
wuziran <wuziran@192.168.9.123>
2019-05-14 14:47:55 +0800

    小吴：222222222

--- Credit_Loan/src/main/java/com/p2p/qiyun/wzr/common/ShiroConfiguration.java
+++ Credit_Loan/src/main/java/com/p2p/qiyun/wzr/common/ShiroConfiguration.java
@@ -76,6 +76,13 @@ public class ShiroConfiguration {
         map.put("/shouye.html", "anon");
         map.put("/sendsms", "anon");
         map.put("/smsselect", "anon");
+        
+        map.put("/demo.html", "anon");
+        map.put("/forgetPwd2", "anon");
+        map.put("/pwd3", "anon");
+        map.put("/forgetPwd2.html", "anon");
+        map.put("/forgetPwd3.html", "anon");
+        map.put("/forgetPwd4.html", "anon");
         map.put("/login.html", "anon");
         map.put("/wzz_information.html", "anon");
 
